418562735028ebd66973ea56bce3e11f79a14442
Mufti Alie Satriawan <iyakiwan19@gmail.com>
2023-03-28 08:47:38 +0700

    initial commit

---
+++ app/src/main/java/com/mufti/bangkit/learn/ilt3/example/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mufti.bangkit.learn.ilt3.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
